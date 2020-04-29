238d2b637e883b038e399750f2b7452f967faa57
Josh Mu <mu@joshmu.com>
2020-04-29 19:19:37 +1000

    use environment port variable for heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 const twitter = require('twitter-lite')
 const getTweets = require('./getTweets.js')
