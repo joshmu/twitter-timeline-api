6d91b1ce44e14bf86b6ba476fc92c634acf9835b
Josh Mu <mu@joshmu.com>
2020-09-01 19:15:37 +1000

    :zap: cors whitelist

--- server.js
+++ server.js
@@ -1,47 +1,47 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const express = require("express");
-const app = express();
-const port = process.env.PORT;
-const dev = process.env.NODE_ENV !== "production";
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+const dev = process.env.NODE_ENV !== 'production'
 
-const twitter = require("twitter-lite");
-const getTweets = require("./getTweets.js");
+const twitter = require('twitter-lite')
+const getTweets = require('./getTweets.js')
 
-const cors = require("cors");
+const cors = require('cors')
 
 let whitelist = [
-  "https://joshmu-dance-website.vercel.app/",
-  "http://localhost:3000",
-  "http://127.0.0.1:5500",
-  "https://joshmu.com",
-  "http://joshmu.com",
-];
+  'https://joshmu-dance-website.vercel.app',
+  'http://localhost:3000',
+  'http://127.0.0.1:5500',
+  'https://joshmu.com',
+  'http://joshmu.com',
+]
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
+      callback(null, true)
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error('Not allowed by CORS'))
     }
   },
-};
+}
 
-app.get("/", cors(corsOptions), async (req, res) => {
+app.get('/', cors(corsOptions), async (req, res) => {
   try {
-    const tweets = await getTweets();
-    res.json(tweets);
+    const tweets = await getTweets()
+    res.json(tweets)
   } catch (err) {
-    res.send(err.message);
+    res.send(err.message)
   }
-});
+})
 
 app.listen(port, () =>
   console.log(
     `${
       dev
-        ? "http://localhost:"
-        : "http://mu-twitter-timeline-api.herokuapp.com:"
+        ? 'http://localhost:'
+        : 'http://mu-twitter-timeline-api.herokuapp.com:'
     }${port}`
   )
-);
+)
